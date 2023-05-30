b54a72c074f7c6c135ce9b1502c932ebfb82f346
qiqi.shi <qiqi.shi@qiqishideMacBook-Pro.local>
2023-05-30 13:46:52 +0800

    save owrk

---
+++ lib/sqbase/sq_base_route_item.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class SQBaseRouteItem  {
+  final String routeName;
+  final WidgetBuilder widgetBuilder;
+
+  SQBaseRouteItem({required this.routeName, required this.widgetBuilder});
+
+}
